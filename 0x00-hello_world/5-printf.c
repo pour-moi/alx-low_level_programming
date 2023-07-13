960b22854b7d9ae0ae3100987dd4753ec0590d91
pour-moi <por64moi@gmail.com>
2023-07-13 06:19:42 -0700

    Output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Code that outputs text using "printf"
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main()
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
