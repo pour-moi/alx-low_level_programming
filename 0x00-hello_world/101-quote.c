bfe52d08523798419045328502e5acb68668b2f3
pour-moi <por64moi@gmail.com>
2023-07-13 10:28:48 -0700

    putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,17 @@
 /**
  *main -Entry point
  *
- *Outputting text using "fputs"
+ *Outputting text using "putchar"
  *
  *Return: Always 1 (Sucess)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	for(int i = 0; sentence[i] != '\0'; i++){
+		putchar(sentence[i]);
+	}
 
 	return (1);
 }
