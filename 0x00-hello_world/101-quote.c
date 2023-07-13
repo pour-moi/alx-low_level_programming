9062b199de6a7f0ce460ac89799558700624db6b
pour-moi <por64moi@gmail.com>
2023-07-13 07:02:21 -0700

    Output using fputs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main -Entry point
+ *
+ *Outputting text using "fputs"
+ *
+ *Return: Always 1 (Sucess)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar,2015-10-19\n", stdout);
+
+	return(1);
+}
