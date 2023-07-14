15a4a667589f26d77886ba9841831c6cd9425882
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 13:58:26 +0000

    both capital and small

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 97, z = 122;
+	int A = 65, Z = 90;
+
+	int i;
+
+	for (i = a; i <= z; i++)
+	{
+		printf("%c", (char)i);
+
+	}
+
+	for (i = A; i <= Z; i++)
+	{
+		printf("%c", (char)i);
+	}
+	
+	printf("\n");
+	return (0);
+}
