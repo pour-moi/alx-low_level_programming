1f81bc6dbe482c2bb1e29f9bdf93b2aebeadef73
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 16:21:57 +0000

    declared i and j outside the loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,11 @@
 
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (int j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
