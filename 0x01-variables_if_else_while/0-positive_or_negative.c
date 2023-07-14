aceceff869387999b2c7ad20a915d96319fb466c
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 03:52:48 +0000

    +ve or -ve ?

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main()
 	n = rand() - RAND_MAX / 2;
 
     if(n > 0){
-        printf("is positive\n");
+        printf("%d is positive\n",n);
     }else if(n == 0){
-        printf("is zero\n");
+        printf("%d is zero\n", n);
     }else
-        printf("is negative\n");
+        printf("%d is negative\n", n);
 
         return (0);
 }
