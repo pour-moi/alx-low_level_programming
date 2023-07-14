783858eef73f2793b1af8f447f4be8894c284eb9
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 15:07:33 +0000

    moved the barace brackets

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,10 @@ int main(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++){
+	for (i = 0; i < 10; i++)
+	{
 		putchar(i + '0');
-		if(i == 9)
+		if (i == 9)
 			break;
 		putchar(',');
 		putchar(' ');
