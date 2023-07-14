af4c3ce0cd3a6cd7a3fc92b7df2a13f030141dab
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 13:24:45 +0000

    char overflow solved

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,18 @@
 int main(void)
 {
 
-	int n, lastdigit;
+	int n, lastnum;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
+	lastnum = n % 10;
 
-	if (lastdigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
-	else if (lastdigit == 0)
+	if (lastnum > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastnum);
+	else if (lastnum == 0)
 		printf("Last digit of %d is 0 and is 0\n", n);
-	else if (lastdigit < 6 && lastdigit != 0)
-		printf("Last digit of %d is %d and " "is less than 6 and not 0\n", n, lastdigit);
+	else if (lastnum < 6 && lastnum != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
 
 	return (0);
 }
