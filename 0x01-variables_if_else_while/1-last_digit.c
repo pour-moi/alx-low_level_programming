2784798020fe2a597cb87981ca45645cf260e1ee
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 12:52:01 +0000

    whitespace error solved

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /**
- *main -Entry point 
- * 
- * 
+ *main -Entry point
+ *
+ *
  * Return: Always 0 (Sucess)
  */
 
@@ -16,13 +16,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n%10;
+	lastdigit = n % 10;
 
-	if(lastdigit > 5)
+	if (lastdigit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
-	else if(lastdigit == 0)
+	else if (lastdigit == 0)
 		printf("Last digit of %d is 0 and is 0\n", n);
-	else if(lastdigit < 6 && lastdigit != 0)
+	else if (lastdigit < 6 && lastdigit != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 
 	return (0);
