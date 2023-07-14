e3fb407d2240aa861bbfa2189dd802a751b2e0bc
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 14:29:12 +0000

    used putchar to output

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@
 
 int main(void)
 {
-	int i;
+	int numbers[] = {0,1,2,3,4,5,6,7,8,9};
 
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		printf("%d", i);
+		putchar(numbers[i] + '0');
 
 	}
 	printf("\n");
