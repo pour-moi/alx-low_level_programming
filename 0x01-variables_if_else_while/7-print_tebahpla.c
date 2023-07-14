443043aadaf4f529ea28021b0003b44af8379e6c
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 14:38:39 +0000

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 97, z = 122;
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+	return (0);
+}
