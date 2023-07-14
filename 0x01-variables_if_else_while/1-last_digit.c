ebc7476cfddf3b2be4efa42398bdbb555db3d73c
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-14 13:16:03 +0000

    line broken

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 /**
  *main -Entry point
@@ -23,7 +24,7 @@ int main(void)
 	else if (lastdigit == 0)
 		printf("Last digit of %d is 0 and is 0\n", n);
 	else if (lastdigit < 6 && lastdigit != 0)
-		printf("Last digit of %d is %d and " 
+		printf("Last digit of %d is %d and"
 			"is less than 6 and not 0\n", n, lastdigit);
 
 	return (0);
