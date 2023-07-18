9ef42f4ec493ad86be746e48bd8be1b1b333d068
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-18 17:30:38 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - prints last digit of numbers
+ * @x: integer to add
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	 for (i = 0; i < 10; i++)
+	 {
+		for (j = 0; j < 10; j++)
+		{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+						{
+							_putchar(i + '0');
+							_putchar(j + '0');
+							_putchar(':');
+							_putchar(k + '0');
+							_putchar(l + '0');
+							_putchar('\n');
+								if(i == 2 && j == 3 && k == 5 && l == 9){
+									exit(1);
+							}
+						}
+				}
+		}
+		_putchar('\n');
+	}
+}
