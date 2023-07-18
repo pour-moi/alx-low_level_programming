de6bcb2b08e3c0151709dc667672a67f90107f5e
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-18 17:52:08 +0000

    tabs reduced

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- * print_last_digit - prints last digit of numbers
- * @x: integer to add
+ * jack_bauer - hours
+ * 
  *
  * Return: Always 0 (Success)
  */
@@ -12,32 +12,27 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	 for (i = 0; i < 10; i++)
-	 {
-		for (j = 0; j < 10; j++)
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k < 6; k++)
 		{
-				for (k = 0; k < 6; k++)
-				{
-					for (l = 0; l < 10; l++)
-						{
-							_putchar(i + '0');
-							_putchar(j + '0');
-							_putchar(':');
-							_putchar(k + '0');
-							_putchar(l + '0');
-							_putchar('\n');
-								if(i == 2 && j == 3 && k == 5 && l == 9){
-									exit(1);
-							}
-						}
-				}
+			for (l = 0; l < 10; l++)
+			{
+				_putchar(i + '0');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
+				if (i == 2 && j == 3 && k == 5 && l == 9)
+					exit(1);		
+			}
 		}
-		
 	}
+
 }
 
-int main(void)
-{
-    jack_bauer();
-    return (0);
 }
+
