115d13e87d9f9f9027f870d94e5f098d5eb88276
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-18 12:27:24 +0000

    lowercase alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - prints _puthchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return(0);
+	
+}
