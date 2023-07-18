f97bd833efc6f94da9de4aaaa112ae4ee074a1fc
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-18 13:45:35 +0000

    returns absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - absolute value of numebrs
+ * @x: integer to add
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	else if (x > 0)
+		return (x);
+
+	return(0);
+}
