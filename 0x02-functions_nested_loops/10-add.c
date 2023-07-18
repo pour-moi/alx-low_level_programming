c332758b00ea7bcdd741bc51e20dd851fccc0d1e
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-18 18:06:25 +0000

    two parameter's added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 
 /**
  * add - adds two numbers
+ *@x: integer to be added
+ *@y: integer to be added
  *
  * Return: Always 0 (Success)
  */
 
 int add(int x, int y)
 {
-	return x + y;
+	return (x + y);
 }
