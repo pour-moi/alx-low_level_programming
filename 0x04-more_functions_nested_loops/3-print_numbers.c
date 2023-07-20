89289a0781bab24d3e583c3521735be541146690
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-20 13:27:30 +0000

    prints number from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0-9
+ * 
+ * 
+ * Return: 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
