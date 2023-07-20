903b5d029c2a429b9a5a45689453ad47f669d7e6
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-20 15:40:43 +0000

    prints square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints line diagonally
+ * @n:integer to pass
+ *
+ * Return: 0
+*/
+
+void print_square(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
