0c6c195f33b8afc00afb94f96293d8160fe33f81
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-20 15:33:21 +0000

    removed = sign

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,16 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n == 0 || n < 0)
+	if (n <= 0)
 		_putchar('\n');
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-			_putchar('\\');
+		_putchar('\\');
 
 		_putchar('\n');
 	}
