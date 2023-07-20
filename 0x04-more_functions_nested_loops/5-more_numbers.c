6c93d8b6441250e23788468ef6869d6e505bceec
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-20 14:07:32 +0000

    condition added if it is more than 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,14 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+				_putchar('1');
+
+			_putchar((j % 10) + '0');
 		}
 
 		_putchar('\n');
 
 	}
-	_putchar('\n');
 
 }
