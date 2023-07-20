77d875994d2f727d97077f87f0c8c7a0afab5121
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-20 18:27:56 +0000

    minor correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,19 +18,17 @@ int main(void)
 		if (i % 3 == 0)
 		{
 			printf("Fizz ");
-			continue;
 		}
-		if (i % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-			continue;
 		}
-		if (i % 5 == 0 && i % 3 == 0)
+		else if (i % 5 == 0 && i % 3 == 0)
 		{
 			printf("FizzBuzz ");
-			continue;
 		}
-		printf("%d ", i);
+		else
+			printf("%d ", i);
 	}
 
 	printf("\n");
