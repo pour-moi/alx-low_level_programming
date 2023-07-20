04dbac75e1b3f3fdc4ee9908ab33eb6bd996ec66
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-20 13:54:51 +0000

    prints from 0-9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number with out 2 and 4
+ *
+ *
+ * Return: 0
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+}
