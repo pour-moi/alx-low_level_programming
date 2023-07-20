8c82e1f5d88978cf0b4ed092e2642421c356a333
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-20 13:10:26 +0000

    checks numbers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if it is capital letter
+ * @c: integer to pass
+ *
+ * Return: 0
+*/
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == c)
+			return (1);
+	}
+
+	return (0);
+}
