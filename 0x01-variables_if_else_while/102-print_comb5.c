96705966b6ddadca5fdc3dca76e55c8491105eb8
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-22 04:45:10 +0000

    indented the first loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i, j, k, l;
 
-for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -24,7 +24,7 @@ for (i = 0; i < 10; i++)
 					putchar(' ');
 					putchar(k + '0');
 					putchar(l + '0');
-					if (i == 9 && j == 8 && k == 9 && l == 9 )
+					if (i == 9 && j == 8 && k == 9 && l == 9)
 						break;
 					putchar(',');
 					putchar(' ');
