272939a3c6fa8044a981288dc5c3fcc09a9c4e8c
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-24 12:36:22 +0000

    swaps to ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *@a: pointer to pass
+ *@b: pointer to pass
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+
+}
