82de1cb7ec6d03269d8e6d8d9a3b4230a214403a
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-24 21:44:07 +0000

    Copies string from src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - prints everyother character
+ *@dest: pointer to pass
+ *@src: pointer to pass
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+    int i;
+    int size = 0;
+
+    while (src[size] != '\0')
+    {
+        size++;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        dest[i] = src[i];
+    }
+    return (dest);
+}
