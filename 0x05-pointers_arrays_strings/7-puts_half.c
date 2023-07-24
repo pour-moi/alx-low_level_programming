a5d9b1233ed4fcc86c18a75d1e4d7e21b861c9e5
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-24 20:03:24 +0000

    prints half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints everyother character
+ *@str: pointer to pass
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int size = 0;
+	int i;
+    int half_size;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+    half_size = size / 2;
+
+	for (i = half_size; *str != '\0'; str++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
