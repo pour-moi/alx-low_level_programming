aef901ef965a904a4d9fb04bb45f87e7ab201f5e
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-28 02:10:18 +0000

    concates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strncat - function that concatenates two strings.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
+ *
+ *Return: pointer to destination string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, j;
+
+	length = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
