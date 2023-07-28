203683b494220fcf789965e9938194cb64aebc51
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-07-28 02:07:36 +0000

    indented

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int length, j;
+	int length, j;
 
-length = 0;
-while (dest[length] != '\0')
-{
-length++;
-}
-for (j = 0; src[j] != '\0'; j++, length++)
-{
-dest[length] = src[j];
-}
-dest[length] = '\0';
-return (dest);
+	length = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+		dest[length] = '\0';
+
+	return (dest);
 }
