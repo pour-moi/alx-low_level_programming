044b0242530ea44180e019d0bc64b433cc5e51ac
pour-moi <pour64moi@gmail.com>
2023-08-10 18:48:15 +0300

    betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints command line 
+ * main - prints command line
  * @argc: unused argument
  * @argv: the string
  *
  * Return: Always 0
  */
 
-int main(int argc __attribute__((unused)) ,char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",argv[0]);
-	
-	return(0);
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
 
