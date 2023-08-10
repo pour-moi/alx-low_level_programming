92ed0fab2ac5fa6022baf3eb230d895618ffac76
pour-moi <pour64moi@gmail.com>
2023-08-10 17:23:06 +0300

    prints command line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - prints command line 
- * @argc: counts
- * @argv: array
+ * @argc: unused argument
+ * @argv: the string
  *
- * Return - Always 0
- * /
+ * Return: Always 0
+ */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)) ,char *argv[])
 {
-	printf("%s\n",argv);
+	printf("%s\n",argv[0]);
 	
 	return(0);
 }
