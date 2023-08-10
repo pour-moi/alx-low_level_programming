bdf558eb3bd1119b79a3567d8d8770951d0a5182
pour-moi <pour64moi@gmail.com>
2023-08-10 20:52:45 +0300

    subtracts 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,9 @@
  * Return: Always 0
  */
 
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
