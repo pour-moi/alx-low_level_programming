c7843f6b517491f2f9f52f9b19a0059231b2f6f7
pour-moi <pour64moi@gmail.com>
2023-08-12 02:23:52 +0300

    creates array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	Array = malloc(size * c);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (Array == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		Array[i] = c;
