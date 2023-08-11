1888bac0d673e62f16b4dc28baf3ef470b49d9a3
pour-moi <pour64moi@gmail.com>
2023-08-12 02:15:37 +0300

    creates array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+* create_array - creats array and assign it with unique char
+*
+* @size: size of the array
+* @c: character
+*
+* Return: Always 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *Array;
+	unsigned int i;
+
+	Array = malloc(size * c);
+	for (i = 0; i < size; i++)
+	{
+		Array[i] = c;
+	}
+
+	return (Array);
+}
+
