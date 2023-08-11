5c95738a9b39f8e97560496b327d354cc2d2ce3e
pour-moi <pour64moi@gmail.com>
2023-08-11 14:40:43 +0300

    adds integer

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,16 +19,16 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("0\n");
-		return (0);
+		exit(1);
 	}
 
 	for (i = 1; i < argc; i++)
 	{
-	if (!isdigit(*argv[i]))
-	{
-		printf("Error\n");
-		return (1);
-	}
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
 
 	    sum += atoi(argv[i]);
 	}
