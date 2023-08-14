50b23a65b75881a92723c4455c0197448c3c60f4
pour-moi <pour64moi@gmail.com>
2023-08-14 22:51:57 +0300

    Data Types

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog - declare name, owner and age
+* @name: Name of the dog
+* @owner: Owner of the dog
+* @age: Age of the dog
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif
