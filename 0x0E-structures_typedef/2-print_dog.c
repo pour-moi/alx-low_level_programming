14128bd5f0c4dcaac032c1ffda51a3d7ea8d3af9
pour-moi <pour64moi@gmail.com>
2023-08-15 00:18:31 +0300

    Print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		printf("Name: %p\n", d->name);
 	else
 		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: %p\n", d->owner);
 	else
