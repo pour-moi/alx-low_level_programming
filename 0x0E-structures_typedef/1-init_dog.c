5d802f1976d9c3f6077124514afc1b158dd17813
pour-moi <pour64moi@gmail.com>
2023-08-14 23:13:46 +0300

    Structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* init_dog - assigns name, age and owner
+* struct dog d - variable of dog datatype
+*
+* @d: struct variable
+* @name: argument to pass
+* @age: argument to pass
+* @owner: argument to pass
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
