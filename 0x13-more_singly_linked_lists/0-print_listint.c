1dde7fb5de37e8aa8136937d42bbc522570c25c5
pour-moi <pour64moi@gmail.com>
2023-08-29 23:43:56 +0300

    Tried another way

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
+
 /**
-* print_listint - prints n count n list
-* @h: pointer to first
-*Return: pointer
-*/
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
-int i;
-if (h == NULL)
-{
-return (0);
-}
-for (i = 0; h != NULL; i++)
-{
-printf("%d\n", (*h).n);
-h = (*h).next;
-}
-return (i);
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
