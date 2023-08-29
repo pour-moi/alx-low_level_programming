f737077378c822301d68273976e1d1b9e8302b5b
pour-moi <pour64moi@gmail.com>
2023-08-29 23:09:16 +0300

    putchar added

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _putchar(char c);
+
+#endif
---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
